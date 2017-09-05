056990653fb505e3b2ff0cace80da6f268d979ec
ManwarSinghRawat <mann.cloud2017@gmail.com>
2017-09-05 23:43:37 +0800

    Edited manifest

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -6,5 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PalTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
+
     }
 }
